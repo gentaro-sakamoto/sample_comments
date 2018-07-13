5be805b1098e54c58e443a089d8a67b672317ae4
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2018-07-13 15:10:24 +0900

    Set relationship

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,4 @@
 class AdminUser < ApplicationRecord
   has_many :comment_ancestries
+  has_many :comment_nested_sets
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :comment_ancestries
+  has_many :comment_nested_sets
 end
